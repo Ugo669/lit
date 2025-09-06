42f6c3a2b18d053eba9a157ee72787174e29204d
ugo669 <theoboy669@gmail.com>
2025-09-06 16:11:06 -0700

    photo

--- my_portfolio/main.py
+++ my_portfolio/main.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 from PIL import Image
+import os
 
 
 
@@ -11,7 +12,7 @@ theme = st.sidebar.selectbox("select  theme", [
 st.title("theophilus  ugochukwu sylvanus")
 st.subheader("Data Scientist | machine learning engineer | Network  Engineer | web developer | Cyber  security  enthusiast | ")
 st.write("###")
-profile = Image.open("2.jpg")
+profile = Image.open(os.path.join(r'C:\project\lit\my_portfolio\2.jpg'))
 st.image(profile, width=200)
 column1, column2, column3 = st.columns(3, gap="large")
 column1.write("#####")
