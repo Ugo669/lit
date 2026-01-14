072afc98afe42a812c31cd298fb1444986e3a0be
ugo669 <theoboy669@gmail.com>
2026-01-14 23:29:47 +0100

    working now

--- my_portfolio/main.py
+++ my_portfolio/main.py
@@ -10,38 +10,42 @@ st.set_page_config(page_title="ugochi stanley", page_icon=":toda", layout="wide"
 st.title("theophilus  ugochukwu sylvanus")
 st.subheader("Data Scientist | machine learning engineer | Network  Engineer | web developer | Cyber  security  enthusiast | ")
 st.write("###")
-#profile = Image.open(os.path.join("images", "2.jpg"), )
-#st.image(profile, width=200)
-column1, column2, column3 = st.columns(3, gap="large")
-column1.write("#####")
-column1.header("About me")
-column1.write("#")
-column1.write(" I am a data scientist and a machine learning engineer with strong background in network engineering and web development. I have a passion for leveraging data  to drive insights, solve complex problems,  and create innovative solutions. with a understand of statistics and programming.")
 
-column2.write("#####")
 
-column2.header("Skills")
-def skills_progress():
-    skills_progress = {
-        "python 90%": 90,
-        "Rust 60%": 60,
-        "C++ 40%": 40,
-        "SQL 70%": 70,
-        "HTML/CSS 80%":80,
-        "Javascript 30%": 30,
-          "Machine learning 70%": 70,
-          "Data analysis 80%":80,
-          }
-    return skills_progress
 
-for skill, progress in skills_progress().items():
-    column2.write(f"{skill}")
-    column2.progress(progress)
+col1, col2, col3= st.columns(3, gap ="large")
+def projects():
+    col1.header("-Some Awesome Works-")
+    #col1.image()
+    col1.subheader("Autormated Decission Making machine")
+    col1.write(" a responsive machine that could help make decission" \
+    "by recomending music, outfits, videos and even what to eat")
+    col1.button("view project")
+    st.write("###")
+    #col1.image()
+    col1.header("The scrapper")
+    col1.write
 
-column3.write("#####")
-column3.header("Projects")
-column3.link_button("1. Automated Decision Making","www.github.com/ugo669/Automated-DM")
-column3.write("this project with simple decision making. Such as which music to  listen to, what movie to watch, what's the matching out to wear. All according to what mood you are in. ")
+
+projects()
+
+st.write("###")
+
+
+col2.header("-who i work with-")
+
+def about():
+    col3.header("About Me")
+    col3.write("I am theophilus ugochukwu sylvanus a developer whom is passion driven" \
+    "with over 5 years of eperience in the development space. Building scalable and user friendly software that solves real life problems in business enterprisses, " \
+    "health wise and in the tech field  " \
+    "" \
+    " ")
+about()
+
+
+
+
 
 st.write("####")
 
