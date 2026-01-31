9374101916824b9406e95088a2e805f94ad54771
ugo669 <theoboy669@gmail.com>
2026-01-31 19:52:08 +0100

    semi working

--- my_portfolio/main.py
+++ my_portfolio/main.py
@@ -14,28 +14,29 @@ st.write("###")
 
 
 col1, col2, col3= st.columns(3, gap ="large")
-def projects():
+def projects(col1):
     col1.header("-Some Awesome Works-")
     #col1.image()
     col1.subheader("Autormated Decission Making machine")
     col1.write(" a responsive machine that could help make decission" \
     "by recomending music, outfits, videos and even what to eat")
-    col1.button("view project")
-    st.write("###")
+    col1.link_button("view project")
+    col1.write("###")
     #col1.image()
-    col1.header("The scrapper")
-    col1.write("A command line web scrapping tool")
+    col1.subheader("The scrapper")
+    col1.write("A python command line web scrapping tool that aids in advance web scrapping")
     col1.button("view project")
-
     st.write("###")
-
     #col1.image
-    col1.header("Aladin")
-    col1.write("a scalable e-commerse website")
-    col1.button("view project")
+    col1.subheader("Aladin")
+    col1.write("a scalable e-commerse website built with django framework")
+    url = "github.com"
+    col1.link_button("view project",url)
 
 
-projects()
+    
+
+projects(col1)
 
 st.write("###")
 
@@ -61,3 +62,4 @@ st.header("contact")
 st.write("phone: 0901 524 3199")
 st.write("Email: theoboy669@gmail.com")
 st.write("github: www.github.com/Ugo669")
+
